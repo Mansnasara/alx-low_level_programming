7827a80f8c3751f866029398a9187bd717b0a22d
Mansnasara <agbemvictor@aol.com>
2022-09-23 07:41:13 -0700

    hot

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,39 +12,12 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	int a = 0, b = 0, c = 0, r = 0, lim;
-
-	while (s1[a])
+	while ((*s1 && *s2) && (*s1 == *s2))
 	{
-		a++;
-	}
-
-	while (s2[b])
-	{
-		b++;
-	}
-
-	if (a <= b)
-	{
-		lim = a;
+		s1++;
+		s2++;
 
 	}
-	else
-	{
-		lim = b;
-	}
-	while (c <= lim)
-	{
-		if (s1[c] == s2[c])
-		{
-			c++;
-			continue;
-		}
-		else
-		{
-			r = s1[c] - s2[c];
-			break;
-		}
 
-		c++;
-	}
+	return (*s1 - *s2);
+}
